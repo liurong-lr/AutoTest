5e6dcee38dd7f457ef77ecb7a6bb3ad55eb0dfa0
liurong <526835657@qq.com>
2019-12-10 21:57:39 +0800

    groups

---
+++ Chapter5/src/main/java/com/couser/testng/groups/GroupsOnMethod.java
@@ -0,0 +1,38 @@
+package com.couser.testng.groups;
+
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+public class GroupsOnMethod {
+    @Test(groups = "server")
+    public void test1(){
+        System.out.println("这是服务器端组的测试方法111");
+
+    }
+    @Test(groups = "server")
+    public void test2(){
+        System.out.println("这是服务器端组的测试方法222");
+
+    }
+
+    @Test(groups = "client")
+    public void test3(){
+        System.out.println("这是客户端端组的测试方法333");
+
+    }
+
+    @Test(groups = "client")
+    public void test4(){
+        System.out.println("这是客户端端组的测试方法444");
+
+    }
+    @BeforeGroups("server")
+    public void beforeGroupsOnServer(){
+        System.out.println("z这是服务端组运行之前运行的方法");
+    }
+    @AfterGroups("server")
+    public void afterGroupsOnServer(){
+        System.out.println("z这是服务端组运行之后运行的方法");
+    }
+}
