9f65f698dcb645ce55d79029dda16614887845a8
liurong <526835657@qq.com>
2019-12-12 10:43:35 +0800

    dataprovider

---
+++ Chapter5/src/main/java/com/couser/testng/paramter/ParamterTest.java
@@ -0,0 +1,17 @@
+package com.couser.testng.paramter;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParamterTest {
+
+    @Test
+    @Parameters({"name","age"})
+    public void paramTest1(String name ,int age){
+
+        System.out.println("name="+name+","+"age="+age);
+
+    }
+
+
+}
